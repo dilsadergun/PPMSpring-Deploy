c5cadabe24a6595b9a8067ea98def8701b568585
Dilsad Ergun <dilsad.ergun@siemens.com>
2020-04-06 13:18:43 +0300

    react deploy

---
+++ src/main/resources/static/precache-manifest.9fa7a89479df44545eb220f353dfe48c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d51ddda9e4d5a9ad6b23a970d184ba4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e940be4e325dd94d895",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4aa42054646d8aa36900",
+    "url": "/static/css/main.86ded62d.chunk.css"
+  },
+  {
+    "revision": "7e940be4e325dd94d895",
+    "url": "/static/js/2.bf50da9d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.bf50da9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4aa42054646d8aa36900",
+    "url": "/static/js/main.eb4cd717.chunk.js"
+  },
+  {
+    "revision": "dbdd831f4424d1730cf6",
+    "url": "/static/js/runtime-main.d22e0a56.js"
+  }
+]);
